c12866e0ac92e158d340195236c750918a95df56
Radriarr <79597127+Radriarr@users.noreply.github.com>
2021-03-06 02:17:03 -0500

    v1.5 update

--- morse_project/morseMain.py
+++ morse_project/morseMain.py
@@ -1,18 +1,31 @@
-# v 0.1, Keyboard-to-morse
-#Imports
+#### Update Log ####
+# 2/27/20: v0.1, Basic sound onKeypress
+# 3/3/20: v1.0, Added dot/dash soundfiles
+# 3/6/2020: v1.5, Replaced soundfiles w/ internally generated sine wave
+
 #import keyboard
 from pynput.keyboard import Key, Listener
 from playsound import playsound
+from pysinewave import SineWave
 import time
+
+sinewave = SineWave(pitch = 15)
+
 
 def on_press(key):
     if key == Key.left:
-        playsound("dot_600.wav")
+        sinewave.play() 
+        time.sleep(0.1)
+        sinewave.stop()
+        #playsound("dot_600.wav"),v1.0
         #print('.')
     elif key == Key.right:
-        playsound("dash_600.wav")
+        sinewave.play()
+        time.sleep(0.2)
+        sinewave.stop()
+        #playsound("dash_600.wav"), v1.0
         #print('-')
-        #playsound("morse_dash.mp3")
+        #playsound("morse_dash.mp3"), OLD
 
 def on_release(key):
     if key == Key.esc:
